96f03482415103f82905dcc456f8f8580cded29a
Lokesh <lokeshchauhan7729@gmail.com>
2025-11-10 14:20:52 +0530

    sum inside array

---
+++ javascript/2darray.js
@@ -0,0 +1,13 @@
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+];
+
+for (let i = 0; i < arr.length; i++) {
+  let sum = 0;
+  for (let j = 0; j < arr[i].length; j++) {
+    sum = sum + arr[i][j] 
+    // console.log(sum)
+}
+console.log(sum)
+}
